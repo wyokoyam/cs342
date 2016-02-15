f96a93bf9a07e3c72646ac8362fa4a58d7020c4c
Blaise <wyokoyam@macalester.edu>
2016-02-15 04:22:28 -0600

    getting going on challenge 3

--- cryptopals/challenge3.c
+++ cryptopals/challenge3.c
@@ -1,4 +1,40 @@
 //Blaise Yokoyama
 //Challenge 3
 
+//Trying to XOR a string with a single character
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "crypto-utils.h"
+#include "crypto-utils.c"
+#include "base64.h"
+
+// Compile with: gcc challenge3.c base64.c crypto-utils.c -g -o challenge3
+
+static char* sInputStr1 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+
+int main(int argc, char *argv[]) {
+
+  int bytesLen1 = 0;
+  char *bytes1 = hexStrToBytes(sInputStr1, &bytesLen1);
+  if (!bytes1) {
+    printf("Failure! Couldn't convert hex to bytes.\n");
+    return 1;
+  }
+
+  char *result = malloc(bytesLen1);
+
+  //XOR against bytes, must convert all ascii values of hex possibilities to bytes
+  //worried about memory here?
+
+  //TODO
+  for(loop through all hex chars in bytes){
+  	for(int x = 0; x<bytesLen1;x++){
+  		result[x] = bytes1[x] ^ ;
+  	}
+
+  	printf("%s \n", result);
+  }
+  return 0;
+}
