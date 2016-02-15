ad505518624d8627960f020fe08cde86026af08b
Blaise <wyokoyam@macalester.edu>
2016-02-15 15:31:58 -0600

    tinker

--- cryptopals/challenge3.c
+++ cryptopals/challenge3.c
@@ -40,32 +40,23 @@ int main(int argc, char *argv[]) {
     int *score = malloc(result); //unsure about how big this should be
     for (int i = 0; result[i] != 0; i++){ 
     	if(result[i] == "e" || result[i] == "E"){
-    		score += 12;
-    	}
+    		score += 12;}
     	if(result[i] == "t" || result[i] == "T"){
-    		score += 9;
-    	}
+    		score += 9;}
     	if(result[i] == "a" || result[i] == "A"){
-    		score += 8;
-    	}
+    		score += 8;}
     	if(result[i] == "o" || result[i] == "O"){
-    		score += 8;
-    	}
+    		score += 8;}
     	if(result[i] == "i" || result[i] == "I"){
-    		score += 7;
-    	}
+    		score += 7;}
     	if(result[i] == "n" || result[i] == "N"){
-    		score += 7;
-    	}
+    		score += 7;}
     	if(result[i] == "s" || result[i] == "S"){
-    		score += 6;
-    	}
+    		score += 6;}
     	if(result[i] == "r" || result[i] == "R"){
-    		score += 6;
-    	}
+    		score += 6;}
     	if(result[i] == "h" || result[i] == "H"){
-    		score += 6;
-    	}
+    		score += 6;}
     }
     if(score > topScore){
     	topScore = score;
