a4e769fe3a7c88705bb67ce751d3a960fa4963ba
Blaise <blaise@localhost.localdomain>
2016-02-22 15:56:44 -0600

    terminating characters are troublesome...

--- cryptopals/challenge5.c
+++ cryptopals/challenge5.c
@@ -17,10 +17,10 @@
 
 // Compile with: gcc challenge5.c base64.c crypto-utils.c -g -o challenge5
 
-static char* sInputStr1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal\0";
+static char* sInputStr1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 
 static char* sOutputStr = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f\0";
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
 char* encodeWithCipher( char *inputStr, char *cipher) {
     int strLen = strlen(inputStr);
@@ -45,7 +45,7 @@ int main(int argc, char *argv[]) {
   }
 
   char *string = (char*) sInputStr1;
-  int bytesLen = strlen(sInputStr1);
+  bytesLen = strlen(sInputStr1);
   char *cipher = (char*) "ICE"; //unsure here
   char *result = encodeWithCipher(string, cipher);
 
