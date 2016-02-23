5e5e6ebea6eab7e6155e8cfe211d9d83b8ed9c5f
Blaise <blaise@localhost.localdomain>
2016-02-22 21:47:36 -0600

    IT ALL WORKSSSSSSSS. IT FINALLY WORKSSSSSSSS!

--- cryptopals/challenge5.c
+++ cryptopals/challenge5.c
@@ -19,8 +19,8 @@
 
 static char* sInputStr1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 
-static char* sOutputStr = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
-a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+static char* sOutputStr = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"
+"a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
 
 char* encodeWithCipher( char *inputStr, char *cipher) {
     int strLen = strlen(inputStr);
@@ -38,7 +38,7 @@ char* encodeWithCipher( char *inputStr, char *cipher) {
 int main(int argc, char *argv[]) {
 
   int bytesLen = 0;
-  char *bytes = hexStrToBytes(sInputStr1, &bytesLen);
+  char *bytes = hexStrToBytes(sOutputStr, &bytesLen);
   if (!bytes) {
     printf("Failure! Couldn't convert hex to bytes.\n");
     return 1;
@@ -46,9 +46,19 @@ int main(int argc, char *argv[]) {
 
   //char *string = (char*) sInputStr1;
   char *cipher = (char*) "ICE"; //unsure here
-  char *result = encodeWithCipher(bytes, cipher);
+  char *result = encodeWithCipher(sInputStr1, cipher);
 
-  //create byte to hex?
+
+  int errors = 0;
+  //compare output string bytes to my encoded byte string
+  for(int x = 0; x<bytesLen; x++){
+    if(bytes[x] != result[x]){
+      errors += 1;
+    }
+  }
+  if(errors == 0){
+    printf("%s\n", "Passed the test");
+  }
 
   printf("%s \n", result);
 
